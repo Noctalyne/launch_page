35be8b8cd4c3cda1b633301eb4dccbc9ab0b12cc
Noctalyne <133142273+Noctalyne@users.noreply.github.com>
2024-07-01 22:08:34 +0200

    maj code js

--- script/launch.js
+++ script/launch.js
@@ -18,17 +18,17 @@ let message = ["E","n","t","r","e","r"]
 let texte = "";
 
 function titre() {
-    // Voir si récupère par classe ou par balise
-    // let cible = document.getElementsByClassName("msg");
     let cible = document.querySelector("h1");
     for (let i = 0; i < message.length; i++) {
-        texte = message[i];
+        texte = texte + message[i];
         
     };
 
     cible.texContent = texte;
-    // cible.textContent = "texte";
-    // texte = texte + 
+
+    // console.log(texte);
+    
+
 
 };
 
