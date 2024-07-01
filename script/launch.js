16d02e7522a9297a465c3ce67900cb4037f67f40
Noctalyne <133142273+Noctalyne@users.noreply.github.com>
2024-07-01 18:11:15 +0200

    début création apparition texte + modification css

--- script/launch.js
+++ script/launch.js
@@ -1,16 +1,40 @@
 
 // récupère le canva par sa classe
-let grad_effect = document.getElementsByClassName("gradient");
+// let grad_effect = document.getElementsByClassName("gradient");
 
-let base = 0;
-let rotation = base + 10;
 
-// pour tester
-
-setInterval(crearot, 100);
-
-function crearot() {
+// Fonction qui gère la rotation
+let degre = 0;
+function rotation() {
     let btn = document.querySelector("button");
-    btn.setAttribute("style", "transform: rotate(" + base + "deg);");
-    base = base + rotation;
+    btn.setAttribute("style", "transform: rotate(" + degre + "deg);");
+    degre = degre +10 ;
 };
+
+
+
+// Fonction pour ecrire le message
+let message = ["E","n","t","r","e","r"]
+let texte = "";
+
+function titre() {
+    // Voir si récupère par classe ou par balise
+    // let cible = document.getElementsByClassName("msg");
+    let cible = document.querySelector("h1");
+    for (let i = 0; i < message.length; i++) {
+        texte = message[i];
+        
+    };
+
+    cible.texContent = texte;
+    // cible.textContent = "texte";
+    // texte = texte + 
+
+};
+
+
+// Utilisation des fonctions
+
+setInterval(rotation, 100);
+
+setInterval(titre, 100);
