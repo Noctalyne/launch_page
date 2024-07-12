ef1040e86decb1417bd1fb2e759c516cf78cd4d2
Noctalyne <133142273+Noctalyne@users.noreply.github.com>
2024-07-12 14:43:56 +0200

    maj js + css

--- script/script.js
+++ script/script.js
@@ -1,6 +1,20 @@
 // Déclaration du message que je veux afficher
 let message = "ENTRER";
 
+// Déclaration des constantes 
+const bouton = document.getElementById("load");
+
+const msg = document.getElementById("msg");
+
+
+// function global() {
+//     if (msg == message) {
+//         afficherTexte(message);
+//     } 
+// }
+// global();
+
+
 
 afficherTexte(message);
 
@@ -9,38 +23,35 @@ afficherTexte(message);
 function afficherTexte(message) {
     let i = 0;
 
-    let cible = document.getElementById("msg");
+    // let cible = document.getElementById("msg");
 
     const intervalle = setInterval(() => {
         if (i < message.length) {
-            cible.innerHTML += message[i];
+            msg.innerHTML += message[i];
             i++;
-        } else if (cible.textContent.length = message.length) {
+        } else if (msg.textContent.length = message.length) {
             clearInterval(intervalle);
 
+            for (i=200; i < 1200; i+200) {
+                console.log("i = " + i);
+            }
 
-            premier = setTimeout(() => {
-                cible.setAttribute("class", "effet_on");
-                clearTimeout(premier);
-            }, 200);
+            effet_activer(200);
+            effet_desactiver(400);
 
-            
-            deux = setTimeout(() => {
-                cible.setAttribute("class", "effet_off");
-                clearTimeout(deux);
-            }, 400);
+            effet_activer(600);
+            effet_desactiver(800);
 
+            effet_activer(1000);
+            effet_desactiver(1200);
 
-            trois = setTimeout(() => {
-                cible.setAttribute("class", "effet_on");
-                clearTimeout(trois);
-            }, 600);
 
             setTimeout(() => {
-                cible.removeAttribute("class");
-                cible.innerHTML = "";
+                msg.removeAttribute("class");
+                bouton.setAttribute("class", "load");
+                msg.innerHTML = "";
                 afficherTexte(message);
-            }, 1000);
+            }, 5000);
 
         }
 
@@ -48,18 +59,38 @@ function afficherTexte(message) {
 
 }
 
+function effet_activer(temps) {
+    const activ = setTimeout(() => {
+        msg.setAttribute("class", "effet_on");
+        bouton.setAttribute("class", "load _glow");
+        clearTimeout(activ);
+    }, temps);
+}
+
+function effet_desactiver(temps) {
+    deux = setTimeout(() => {
+        msg.setAttribute("class", "effet_off");
+        bouton.setAttribute("class", "load");
+        clearTimeout(deux);
+    }, temps); // changer le délai +200
+}
 
 
 // Fonction qui gère la rotation
 let degre = 0;
 setInterval(() => {
 
-    let btn = document.getElementById("load");
-    btn.setAttribute("style", "transform: rotate(" + degre + "deg);");
+    // let btn = document.getElementById("load");
+
+    bouton.setAttribute("style", "transform: rotate(" + degre + "deg);");
     degre = degre + 10;
 
     // Bloque à 360° pour éviter qu'il continue à l'infini
     if (degre >= 361) {
         degre = 0;
+        // btn.setAttribute("class", "load _glow");
     }
-}, 100);
+    // else {
+    //     btn.setAttribute("class", "load _glow");
+    // }
+}, 200);
